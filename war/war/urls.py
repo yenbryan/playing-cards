3c46518c1321b3a3c7b7a8916085d98f4067a310
Rudy Mutter <rudy@yetihq.com>
2014-10-12 19:40:48 -0700

    upgrade to django 1.7

--- war/war/urls.py
+++ war/war/urls.py
@@ -28,7 +28,7 @@ urlpatterns = patterns('',
     url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
     # Support old style base36 password reset links; remove in Django 1.7
     url(r'^reset/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        'django.contrib.auth.views.password_reset_confirm_uidb36'),
+        'django.contrib.auth.views.password_reset_confirm'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         'django.contrib.auth.views.password_reset_confirm',
         name='password_reset_confirm'),
